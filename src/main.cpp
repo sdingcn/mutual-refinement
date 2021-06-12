5349b22de11dba288309a25251f4f80e8290eb2b
Shuo Ding <sdingcn@outlook.com>
2021-06-12 16:21:19 -0400

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -15,99 +15,11 @@
 #include "parser/parser.h"
 #include "graph/graph.h"
 
-void test() {
-	/*
-	 * D[0] -> D[0] D[0] | ([1] Dc[3] | epsilon
-	 * Dc[3] -> D[0] )[2]
-	 *
-	 * 0 -> 0 0 | 1 3 | epsilon
-	 * 3 -> 0 2
-	 *
-	 * 10 is a negligible terminal. We have 0 -> 10.
-	 */
-	Grammar gm;
-	gm.terminals.insert(1);
-	gm.terminals.insert(2);
-	gm.terminals.insert(10);
-	gm.nonterminals.insert(0);
-	gm.nonterminals.insert(3);
-	gm.emptyProductions.push_back(0);
-	gm.unaryProductions.push_back(std::make_pair(0, 10));
-	gm.binaryProductions.push_back(std::make_pair(0, std::make_pair(0, 0)));
-	gm.binaryProductions.push_back(std::make_pair(0, std::make_pair(1, 3)));
-	gm.binaryProductions.push_back(std::make_pair(3, std::make_pair(0, 2)));
-	gm.startSymbol = 0;
-	gm.fillInv(11);
-	/*
-	 *  1->(4)-10->(5)-2->
-	 *  |                 \    -1->
-	 *  |                  \  | /
-	 * (0) --1--> (1) --2--> (2) --2--> (3)
-	 *  |                     |
-	 *   ----------1--------->
-	 */
-	Graph gh(gm, 6);
-	gh.addEdge(make_fast_triple(0, 1, 4));
-	gh.addEdge(make_fast_triple(4, 10, 5));
-	gh.addEdge(make_fast_triple(5, 2, 2));
-	gh.addEdge(make_fast_triple(0, 1, 1));
-	gh.addEdge(make_fast_triple(1, 2, 2));
-	gh.addEdge(make_fast_triple(2, 1, 2));
-	gh.addEdge(make_fast_triple(2, 2, 3));
-	gh.addEdge(make_fast_triple(0, 1, 2));
-	gh.runCFLReachability();
-	for (int i = 0; i < 6; i++) {
-		for (int j = 0; j < 6; j++) {
-			if ((i == j) || (i == 0 && j == 2) || (i == 0 && j == 3) ||
-			    (i == 2 && j == 3) || (i == 4 && j == 5)) {
-				assert(gh.hasEdge(make_fast_triple(i, gm.startSymbol, j)));
-			} else {
-				assert(!gh.hasEdge(make_fast_triple(i, gm.startSymbol, j)));
-			}
-		}
-	}
-	{
-		auto rec1 = gh.getCFLReachabilityEdgeClosure(0, 2);
-		assert(rec1.size() == 5);
-		assert(rec1.count(make_fast_triple(0, 1, 1)) == 1);
-		assert(rec1.count(make_fast_triple(1, 2, 2)) == 1);
-		assert(rec1.count(make_fast_triple(0, 1, 4)) == 1);
-		assert(rec1.count(make_fast_triple(4, 10, 5)) == 1);
-		assert(rec1.count(make_fast_triple(5, 2, 2)) == 1);
-	}
-	{
-		auto rec2 = gh.getCFLReachabilityEdgeClosure(2, 3);
-		assert(rec2.size() == 2);
-		assert(rec2.count(make_fast_triple(2, 1, 2)) == 1);
-		assert(rec2.count(make_fast_triple(2, 2, 3)) == 1);
-	}
-	{
-		auto rec3 = gh.getCFLReachabilityEdgeClosure(2, 2);
-		assert(rec3.size() == 0);
-	}
-}
+void test();
 
-void dumpVirtualMemoryPeak() {
-        std::ifstream in("/proc/self/status");
-        std::string line;
-        while(getline(in, line)) {
-                std::istringstream sin(line);
-                std::string tag;
-                sin >> tag;
-                if (tag == "VmPeak:") {
-                        std::cout << line << std::endl;
-                        return;
-                }
-        }
-}
+void dumpVirtualMemoryPeak();
 
-void printUsage(const char *name) {
-	std::cerr << "Usage:" << std::endl
-		<< '\t' << name << " test" << std::endl
-		<< '\t' << name << " pa-ssss" << " <graph-file-path>" << std::endl
-		<< '\t' << name << " pa-all" << " <graph-file-path>" << std::endl
-		<< '\t' << name << " bp" << " <graph-file-path>" << std::endl;
-}
+void printUsage(const char *name);
 
 #define check_resource(str) do {\
 	auto end = std::chrono::steady_clock::now();\
@@ -279,4 +191,98 @@ END:;
 	} else {
 		printUsage(argv[0]);
 	}
+}
+
+void test() {
+	/*
+	 * D[0] -> D[0] D[0] | ([1] Dc[3] | epsilon
+	 * Dc[3] -> D[0] )[2]
+	 *
+	 * 0 -> 0 0 | 1 3 | epsilon
+	 * 3 -> 0 2
+	 *
+	 * 10 is a negligible terminal. We have 0 -> 10.
+	 */
+	Grammar gm;
+	gm.terminals.insert(1);
+	gm.terminals.insert(2);
+	gm.terminals.insert(10);
+	gm.nonterminals.insert(0);
+	gm.nonterminals.insert(3);
+	gm.emptyProductions.push_back(0);
+	gm.unaryProductions.push_back(std::make_pair(0, 10));
+	gm.binaryProductions.push_back(std::make_pair(0, std::make_pair(0, 0)));
+	gm.binaryProductions.push_back(std::make_pair(0, std::make_pair(1, 3)));
+	gm.binaryProductions.push_back(std::make_pair(3, std::make_pair(0, 2)));
+	gm.startSymbol = 0;
+	gm.fillInv(11);
+	/*
+	 *  1->(4)-10->(5)-2->
+	 *  |                 \    -1->
+	 *  |                  \  | /
+	 * (0) --1--> (1) --2--> (2) --2--> (3)
+	 *  |                     |
+	 *   ----------1--------->
+	 */
+	Graph gh(gm, 6);
+	gh.addEdge(make_fast_triple(0, 1, 4));
+	gh.addEdge(make_fast_triple(4, 10, 5));
+	gh.addEdge(make_fast_triple(5, 2, 2));
+	gh.addEdge(make_fast_triple(0, 1, 1));
+	gh.addEdge(make_fast_triple(1, 2, 2));
+	gh.addEdge(make_fast_triple(2, 1, 2));
+	gh.addEdge(make_fast_triple(2, 2, 3));
+	gh.addEdge(make_fast_triple(0, 1, 2));
+	gh.runCFLReachability();
+	for (int i = 0; i < 6; i++) {
+		for (int j = 0; j < 6; j++) {
+			if ((i == j) || (i == 0 && j == 2) || (i == 0 && j == 3) ||
+			    (i == 2 && j == 3) || (i == 4 && j == 5)) {
+				assert(gh.hasEdge(make_fast_triple(i, gm.startSymbol, j)));
+			} else {
+				assert(!gh.hasEdge(make_fast_triple(i, gm.startSymbol, j)));
+			}
+		}
+	}
+	{
+		auto rec1 = gh.getCFLReachabilityEdgeClosure(0, 2);
+		assert(rec1.size() == 5);
+		assert(rec1.count(make_fast_triple(0, 1, 1)) == 1);
+		assert(rec1.count(make_fast_triple(1, 2, 2)) == 1);
+		assert(rec1.count(make_fast_triple(0, 1, 4)) == 1);
+		assert(rec1.count(make_fast_triple(4, 10, 5)) == 1);
+		assert(rec1.count(make_fast_triple(5, 2, 2)) == 1);
+	}
+	{
+		auto rec2 = gh.getCFLReachabilityEdgeClosure(2, 3);
+		assert(rec2.size() == 2);
+		assert(rec2.count(make_fast_triple(2, 1, 2)) == 1);
+		assert(rec2.count(make_fast_triple(2, 2, 3)) == 1);
+	}
+	{
+		auto rec3 = gh.getCFLReachabilityEdgeClosure(2, 2);
+		assert(rec3.size() == 0);
+	}
+}
+
+void dumpVirtualMemoryPeak() {
+        std::ifstream in("/proc/self/status");
+        std::string line;
+        while(getline(in, line)) {
+                std::istringstream sin(line);
+                std::string tag;
+                sin >> tag;
+                if (tag == "VmPeak:") {
+                        std::cout << line << std::endl;
+                        return;
+                }
+        }
+}
+
+void printUsage(const char *name) {
+	std::cerr << "Usage:" << std::endl
+		<< '\t' << name << " test" << std::endl
+		<< '\t' << name << " pa-ssss" << " <graph-file-path>" << std::endl
+		<< '\t' << name << " pa-all" << " <graph-file-path>" << std::endl
+		<< '\t' << name << " bp" << " <graph-file-path>" << std::endl;
 }
