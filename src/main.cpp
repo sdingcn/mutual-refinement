7cd8ec05ad0449d95101c9fe75832912185fdcbb
Shuo Ding <sdingcn@outlook.com>
2021-06-22 10:50:42 -0400

    complete frontend for CFL

--- src/main.cpp
+++ src/main.cpp
@@ -25,143 +25,165 @@ void printUsage(const char *name);
 #define check_resource(str) do {\
 	auto end = std::chrono::steady_clock::now();\
 	std::chrono::duration<double> elapsed_seconds = end - start;\
-	std::cout << "[--- begin resource check: " << str << " ---]" << std::endl;\
-	std::cout << "Time (Seconds): " << elapsed_seconds.count() << std::endl;\
+	std::cout << '{' << std::endl;\
+	std::cout << "    resource check: " << str << std::endl;\
+	std::cout << "    Time (Seconds): " << elapsed_seconds.count() << std::endl;\
 	dumpVirtualMemoryPeak();\
-	std::cout << "[--- end resource check ---]" << std::endl;\
+	std::cout << '}' << std::endl;\
 } while (0)
 
 int main(int argc, char *argv[]) {
 	auto start = std::chrono::steady_clock::now();
 	if (argc == 2 && argv[1] == std::string("test")) {
 		test();
-	} else if (argc == 3 && argv[1] == std::string("pa-cfl-single")) {
-		// read data
+	} else if (argc == 4 && argv[1] == std::string("pa-cfl")) {
+		// parse data
 		const std::tuple<std::map<std::string, int>, std::map<std::string, int>, std::vector<long long>, int, std::vector<Grammar>>
-			data = parsePAGraph(argv[2]);
-		check_resource("completed parsing");
+			data = parsePAGraph(argv[3]);
+		check_resource("Parsing");
 
+		// obtain references to the original data
 		const auto &v_map = std::get<0>(data); // original vertex name -> number
 		const auto &l_map = std::get<1>(data); // original edge label -> number
 		const auto &edges = std::get<2>(data);
-		const int nv = std::get<3>(data);
+		const int &nv = std::get<3>(data);
 		const auto &grammars = std::get<4>(data);
 
-		// construct graphs
-		Graph gh1(grammars[0], nv);
-		gh1.fillEdges(edges);
-		Graph gh2(grammars[1], nv);
-		gh2.fillEdges(edges);
-
-		// first run of CFL reachability
-		gh1.runCFLReachability();
-		gh2.runCFLReachability();
-		check_resource("completed preprocessing");
-
-		// main query loop
-		int totalL1 = 0;
-		int totalL2 = 0;
-		int totalBoolean = 0;
-		int totalECFix = 0;
-		for (int source = 0; source < nv; source++) {
-			std::cout << ">>> Query Progress: " << source << ',' << nv - 1 << std::endl;
-			for (int sink = 0; sink < nv; sink++) {
-				long long e1 = make_fast_triple(source, grammars[0].startSymbol, sink);
-				long long e2 = make_fast_triple(source, grammars[1].startSymbol, sink);
-				bool reach1 = gh1.hasEdge(e1);
-				bool reach2 = gh2.hasEdge(e2);
-				if (reach1) {
-					totalL1++;
+		// CFL1
+		if (argv[2] == std::string("cfl1")) {
+			Graph gh1(grammars[0], nv);
+			gh1.fillEdges(edges);
+			gh1.runCFLReachability();
+			int totalCFL1 = 0;
+			for (int source = 0; source < nv; source++) {
+				for (int sink = 0; sink < nv; sink++) {
+					if (gh1.hasEdge(make_fast_triple(source, grammars[0].startSymbol, sink))) {
+						totalCFL1++;
+					}
 				}
-				if (reach2) {
-					totalL2++;
+			}
+			std::cout << "CFL1: " << totalCFL1 << std::endl;
+			check_resource("CFL1");
+		} else if (argv[2] == std::string("cfl2")) {
+			Graph gh2(grammars[1], nv);
+			gh2.fillEdges(edges);
+			gh2.runCFLReachability();
+			int totalCFL2 = 0;
+			for (int source = 0; source < nv; source++) {
+				for (int sink = 0; sink < nv; sink++) {
+					if (gh2.hasEdge(make_fast_triple(source, grammars[1].startSymbol, sink))) {
+						totalCFL2++;
+					}
 				}
-				if (reach1 && reach2) {
-					totalBoolean++;
-					std::unordered_set<long long> es;
-					{
-						auto c1 = gh1.getCFLReachabilityEdgeClosure(source, sink);
-						auto c2 = gh2.getCFLReachabilityEdgeClosure(source, sink);
-						for (long long e : c1) {
-							if (c2.count(e) == 1) {
-								es.insert(e);
+			}
+			std::cout << "CFL2: " << totalCFL2 << std::endl;
+			check_resource("CFL2");
+		} else if (argv[2] == std::string("bool")){
+			Graph gh1(grammars[0], nv);
+			gh1.fillEdges(edges);
+			gh1.runCFLReachability();
+			Graph gh2(grammars[1], nv);
+			gh2.fillEdges(edges);
+			gh2.runCFLReachability();
+			int totalBoolean = 0;
+			for (int source = 0; source < nv; source++) {
+				for (int sink = 0; sink < nv; sink++) {
+					if (gh1.hasEdge(make_fast_triple(source, grammars[0].startSymbol, sink))
+					 && gh2.hasEdge(make_fast_triple(source, grammars[1].startSymbol, sink))) {
+						totalBoolean++;
+					}
+				}
+			}
+			std::cout << "Boolean: " << totalBoolean << std::endl;
+			check_resource("Boolean");
+		} else if (argv[2] == std::string("apmr")) {
+			int totalAPMR = 0;
+			std::unordered_set<long long> es;
+			for (long long e : edges) {
+				es.insert(e);
+			}
+			while (true) {
+				Graph gh1(grammars[0], nv);
+				gh1.fillEdges(es);
+				gh1.runCFLReachability();
+				auto ec1 = gh1.getCFLReachabilityEdgeClosureAll();
+				Graph gh2(grammars[1], nv);
+				gh2.fillEdges(ec1);
+				gh2.runCFLReachability();
+				auto ec2 = gh2.getCFLReachabilityEdgeClosureAll();
+				if (ec2.size() == es.size()) {
+					for (int source = 0; source < nv; source++) {
+						for (int sink = 0; sink < nv; sink++) {
+							long long e1 = make_fast_triple(source, grammars[0].startSymbol, sink);
+							long long e2 = make_fast_triple(source, grammars[1].startSymbol, sink);
+							if (gh1.hasEdge(e1) && gh2.hasEdge(e2)) {
+								totalAPMR++;
 							}
 						}
 					}
-					while (true) {
-						Graph gha(grammars[0], nv);
-						gha.fillEdges(es);
-						gha.runCFLReachability();
-						if (!(gha.hasEdge(e1))) {
-							break;
-						}
-						auto ca = gha.getCFLReachabilityEdgeClosure(source, sink);
-						Graph ghb(grammars[1], nv);
-						ghb.fillEdges(ca);
-						ghb.runCFLReachability();
-						if (!(ghb.hasEdge(e2))) {
-							break;
-						}
-						auto cb = ghb.getCFLReachabilityEdgeClosure(source, sink);
-						if (cb.size() == es.size()) {
-							totalECFix++;
-							break;
-						} else {
-							es = std::move(cb);
-						}
-					}
+					break;
+				} else {
+					es = std::move(ec2);
 				}
 			}
-		}
-		
-		std::cout << "totalL1: " << totalL1 << std::endl;
-		std::cout << "totalL2: " << totalL2 << std::endl;
-		std::cout << "totalBoolean: " << totalBoolean << std::endl;
-		std::cout << "Total Pairs of ECFix: " << totalECFix << std::endl;
-		check_resource("total");
-	} else if (argc == 3 && argv[1] == std::string("pa-cfl-all")) {
-		// read data
-		const std::tuple<std::map<std::string, int>, std::map<std::string, int>, std::vector<long long>, int, std::vector<Grammar>>
-			data = parsePAGraph(argv[2]);
-		check_resource("completed parsing");
-
-		const auto &v_map = std::get<0>(data); // original vertex name -> number
-		const auto &l_map = std::get<1>(data); // original label name -> number
-		const auto &edges = std::get<2>(data);
-		const int nv = std::get<3>(data);
-		const auto &grammars = std::get<4>(data);
-
-		int totalECFix = 0;
-		std::unordered_set<long long> es;
-		for (long long e : edges) {
-			es.insert(e);
-		}
-		while (true) {
+			std::cout << "All Pairs Mutual Refinement: " << totalAPMR << std::endl;
+			check_resource("All Pairs Mutual Refinement");
+		} else if (argv[2] == std::string("spmr")) {
 			Graph gh1(grammars[0], nv);
-			gh1.fillEdges(es);
-			gh1.runCFLReachability();
-			auto ec1 = gh1.getCFLReachabilityEdgeClosureAll();
+			gh1.fillEdges(edges);
 			Graph gh2(grammars[1], nv);
-			gh2.fillEdges(ec1);
+			gh2.fillEdges(edges);
+
+			gh1.runCFLReachability();
 			gh2.runCFLReachability();
-			auto ec2 = gh2.getCFLReachabilityEdgeClosureAll();
-			if (ec2.size() == es.size()) {
-				for (int source = 0; source < nv; source++) {
-					for (int sink = 0; sink < nv; sink++) {
-						long long e1 = make_fast_triple(source, grammars[0].startSymbol, sink);
-						long long e2 = make_fast_triple(source, grammars[1].startSymbol, sink);
-						if (gh1.hasEdge(e1) && gh2.hasEdge(e2)) {
-							totalECFix++;
+			check_resource("Preprocessing");
+
+			int totalSPMR = 0;
+			for (int source = 0; source < nv; source++) {
+				for (int sink = 0; sink < nv; sink++) {
+					long long e1 = make_fast_triple(source, grammars[0].startSymbol, sink);
+					long long e2 = make_fast_triple(source, grammars[1].startSymbol, sink);
+					if (gh1.hasEdge(e1) && gh2.hasEdge(e2)) {
+						std::unordered_set<long long> es;
+						{
+							auto c1 = gh1.getCFLReachabilityEdgeClosure(source, sink);
+							auto c2 = gh2.getCFLReachabilityEdgeClosure(source, sink);
+							for (long long e : c1) {
+								if (c2.count(e) == 1) {
+									es.insert(e);
+								}
+							}
+						}
+						while (true) {
+							Graph gha(grammars[0], nv);
+							gha.fillEdges(es);
+							gha.runCFLReachability();
+							if (!(gha.hasEdge(e1))) {
+								break;
+							}
+							auto ca = gha.getCFLReachabilityEdgeClosure(source, sink);
+							Graph ghb(grammars[1], nv);
+							ghb.fillEdges(ca);
+							ghb.runCFLReachability();
+							if (!(ghb.hasEdge(e2))) {
+								break;
+							}
+							auto cb = ghb.getCFLReachabilityEdgeClosure(source, sink);
+							if (cb.size() == es.size()) {
+								totalSPMR++;
+								break;
+							} else {
+								es = std::move(cb);
+							}
 						}
 					}
 				}
-				break;
-			} else {
-				es = std::move(ec2);
 			}
+			std::cout << "Single Pair Mutual Refinement: " << totalSPMR << std::endl;
+			check_resource("Single Pair Mutual Refinement");
+		} else {
+			printUsage(argv[0]);
 		}
-		std::cout << "Total Pairs of ECFix: " << totalECFix << std::endl;
-		check_resource("total");
 	} else if (argc == 3 && argv[1] == std::string("bp")) {
 		std::tuple<std::vector<long long>, int, std::pair<int, int>, std::vector<Grammar>> data = parseBPGraph(argv[2]);
 		const auto &edges = std::get<0>(data);
@@ -281,7 +303,7 @@ void dumpVirtualMemoryPeak() {
                 std::string tag;
                 sin >> tag;
                 if (tag == "VmPeak:") {
-                        std::cout << line << std::endl;
+                        std::cout << "    " << line << std::endl;
                         return;
                 }
         }
@@ -290,7 +312,6 @@ void dumpVirtualMemoryPeak() {
 void printUsage(const char *name) {
 	std::cerr << "Usage:" << std::endl
 		<< '\t' << name << " test" << std::endl
-		<< '\t' << name << " pa-cfl-single" << " <graph-file-path>" << std::endl
-		<< '\t' << name << " pa-cfl-all" << " <graph-file-path>" << std::endl
-		<< '\t' << name << " bp" << " <graph-file-path>" << std::endl;
+		<< '\t' << name << " pa-cfl <cfl1|cfl2|bool|apmr|spmr> <graph-file-path>" << std::endl
+		<< '\t' << name << " bp <graph-file-path>" << std::endl;
 }
