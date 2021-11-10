7bd412d1307cc7476e8064adf10ca01db491c522
Shuo Ding <shuo.d@outlook.com>
2021-11-10 15:44:05 -0500

    10G mem limit

--- run-reachability.sh
+++ run-reachability.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ulimit -t 1800 -v 31457280
+ulimit -t 1800 -v 10485760
 
 for dotfile in lcl-exp/taint/normal/*.dot; do
 	./naive "$dotfile" > "results/reachability/naive-$(basename "$dotfile" .dot).res" 2>&1
