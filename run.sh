e9f433cf4fe0983ed702407281b025ec09cca2db
Shuo Ding <sdingcn@outlook.com>
2022-10-29 18:57:27 -0400

    ready.

--- run.sh
+++ run.sh
@@ -3,7 +3,14 @@
 # 1 hour, 10 GB
 ulimit -t 3600 -v 10485760
 
-for graph in exp/graphs/*.dot; do
-	./naive "$graph" > "exp/results/naive-$(basename "$graph" .dot).result" 2>&1
-	./refine "$graph" > "exp/results/refine-$(basename "$graph" .dot).result" 2>&1
+# taint
+for graph in exp/graphs/taint/*.dot; do
+	./main "$graph" naive 0 1 > "exp/results/taint/naive-$(basename "$graph" .dot).result" 2>&1
+	./main "$graph" refine 0 1 > "exp/results/taint/refine-$(basename "$graph" .dot).result" 2>&1
+done
+
+# valueflow
+for graph in exp/graphs/valueflow/*.dot; do
+	./main "$graph" naive 0 0 > "exp/results/valueflow/naive-$(basename "$graph" .dot).result" 2>&1
+	./main "$graph" refine 0 0 > "exp/results/valueflow/refine-$(basename "$graph" .dot).result" 2>&1
 done
