f5a4f208ee1acb103e3963352a4b081dad5df00c
Shuo Ding <sdingcn@outlook.com>
2022-10-30 15:22:34 -0400

    added prints in run.sh

--- run.sh
+++ run.sh
@@ -5,12 +5,16 @@ ulimit -t 3600 -v 10485760
 
 # taint
 for graph in exp/graphs/taint/*.dot; do
+	echo "running naive on $graph"
 	./main "$graph" naive 0 1 > "exp/results/taint/naive-$(basename "$graph" .dot).result" 2>&1
+	echo "running refine on $graph"
 	./main "$graph" refine 0 1 > "exp/results/taint/refine-$(basename "$graph" .dot).result" 2>&1
 done
 
 # valueflow
 for graph in exp/graphs/valueflow/*.dot; do
+	echo "running naive on $graph"
 	./main "$graph" naive 0 0 > "exp/results/valueflow/naive-$(basename "$graph" .dot).result" 2>&1
+	echo "running refine on $graph"
 	./main "$graph" refine 0 0 > "exp/results/valueflow/refine-$(basename "$graph" .dot).result" 2>&1
 done
